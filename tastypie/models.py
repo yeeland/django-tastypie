72edfb417d01bc9ad70433a40f90bfc6ff98b4e2
Vitaly Babiy <vbabiy86@gmail.com>
2013-03-30 12:11:38 -0400

    Merging migration fixes from vbabiy

--- tastypie/models.py
+++ tastypie/models.py
@@ -28,8 +28,7 @@ class ApiAccess(models.Model):
 
 if 'django.contrib.auth' in settings.INSTALLED_APPS:
     import uuid
-    AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
-    
+    from tastypie.compat import AUTH_USER_MODEL
     class ApiKey(models.Model):
         user = models.OneToOneField(AUTH_USER_MODEL, related_name='api_key')
         key = models.CharField(max_length=256, blank=True, default='', db_index=True)
